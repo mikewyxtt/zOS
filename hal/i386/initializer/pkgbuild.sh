e300612757044b1b28ec7f0cc12cdbe3f1875bd2
Mike Wyatt <wyattmich61@gmail.com>
2024-03-02 02:53:22 -0500

    configured for cargo

--- hal/i386/initializer/pkgbuild.sh
+++ hal/i386/initializer/pkgbuild.sh
@@ -5,10 +5,13 @@ build() {
     echo Building $PKGNAME $VERSION
 
     make -j8
+    RUSTFLAGS="-C link-arg=start.o -C link-arg=-Tlink.ld" cargo build --verbose --target=i686.json
+    #RUSTFLAGS="--extern start=start.o" cargo build --verbose --target=i686.json
 }
 
 clean() {
     make clean
+    cargo clean
 }
 
 install() {
