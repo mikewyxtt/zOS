6325453139dc50be01574f6ad3bd7fca47ed8bca
Mike Wyatt <wyattmich61@gmail.com>
2024-03-06 21:15:48 -0500

    parse memory map fro mb2

--- hal/i386/initializer/src/initbootinfo.rs
+++ hal/i386/initializer/src/initbootinfo.rs
@@ -21,6 +21,7 @@ use chimera::hal::boot::bootinfo::BootInfo;
 use chimera::hal::boot::bootinfo::i686::ArchBootInfo;
 use crate::multiboot2::*;
 
+
 pub fn initialize(bootinfo: &mut BootInfo, archbootinfo: &mut ArchBootInfo, multiboot2_info_address: usize) {
     
     bootinfo.early_log_buffer.size = bootinfo.early_log_buffer.buffer.len();
@@ -34,57 +35,92 @@ pub fn initialize(bootinfo: &mut BootInfo, archbootinfo: &mut ArchBootInfo, mult
     }
 
     parse_multiboot_header(bootinfo, archbootinfo, multiboot2_info_address);
-
 }
 
+
 pub fn parse_multiboot_header(bootinfo: &mut BootInfo, archbootinfo: &mut ArchBootInfo, multiboot2_info_address: usize) {
-    archbootinfo.x = 0; // bs entry to hide warning
+    archbootinfo.x = 0;
     
     // pointer to first multiboot tag entry
     let mut tag: *const MultibootTag = (multiboot2_info_address + 8) as *const _;
 
 
     loop {
-        unsafe {
-            match (*tag).type_ {
-                MULTIBOOT_TAG_TYPE_FRAMEBUFFER => {
-                    
-                    let fbtag: *const MultibootTagFramebuffer = core::ptr::from_raw_parts(tag as *const _, (*tag).size as usize);
+        match unsafe { (*tag).type_ } {
+            MULTIBOOT_TAG_TYPE_FRAMEBUFFER => {
+                
+                let fbtag: *const MultibootTagFramebuffer = unsafe { core::ptr::from_raw_parts(tag as *const _, (*tag).size as usize) };
 
-                    if (*fbtag).common.framebuffer_type == 1 {
-                        // Type of 1 means RGB, 2 means EGA text mode (unsupported), 0 means indexed color (unsupported)
-                        bootinfo.framebuffer.enabled = true;
+                if unsafe { (*fbtag).common.framebuffer_type == 1 } {
+                    // Type of 1 means RGB, 2 means EGA text mode (unsupported), 0 means indexed color (unsupported)
+                    bootinfo.framebuffer.enabled = true;
+                    unsafe {
                         bootinfo.framebuffer.addr = (*fbtag).common.framebuffer_addr as usize;
                         bootinfo.framebuffer.width = (*fbtag).common.framebuffer_width;
                         bootinfo.framebuffer.height = (*fbtag).common.framebuffer_height;
                         bootinfo.framebuffer.pitch = (*fbtag).common.framebuffer_pitch;
                         bootinfo.framebuffer.depth = ((*fbtag).common.framebuffer_bpp / 8) as u32;
                         bootinfo.framebuffer.size = (bootinfo.framebuffer.width as u64 * bootinfo.framebuffer.height as u64 * bootinfo.framebuffer.depth as u64) as u64;
-                        
-    
-                        // // Since we have a framebuffer, initialize the console.
-                        bootinfo.console.cursor_pos = 0;
-                        bootinfo.console.line = 0;
-                        bootinfo.console.max_chars = bootinfo.framebuffer.width / 8;
-                        bootinfo.console.max_line = bootinfo.framebuffer.height / 16;
                     }
-                }
+                    
 
-                MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME => {
-                    //serial_log!("Found MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME");
-                }
-
-                MULTIBOOT_TAG_TYPE_END => {
-                    break;
-                }
-
-                // Handle tag types we don't care about
-                _ => {
+                    // Since we have a framebuffer, initialize the console.
+                    bootinfo.console.cursor_pos = 0;
+                    bootinfo.console.line = 0;
+                    bootinfo.console.max_chars = bootinfo.framebuffer.width / 8;
+                    bootinfo.console.max_line = bootinfo.framebuffer.height / 16;
                 }
             }
-            
-            // Index the tag pointer (tag.size) number of bytes forward, ensuring it is 8 byte aligned as per Multiboot 2 spec.
-            tag = (tag as usize + ((*tag).size as usize + 7) & !7) as *const _;
+
+            MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME => {
+                //serial_log!("Found MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME");
+            }
+
+            MULTIBOOT_TAG_TYPE_END => {
+                break;
+            }
+
+            MULTIBOOT_TAG_TYPE_MMAP => {
+                let mmap_tag: *const MultibootTagMmap = unsafe { core::ptr::from_raw_parts(tag as *const _, (*tag).size as usize) };
+
+                let mut i = 0;
+
+                loop {
+                    let mmap = unsafe { &(*mmap_tag).entries[i] as *const MultibootMemoryMap };
+
+                    unsafe {
+                        if (mmap as usize) < (tag as usize + (*tag).size as usize) {
+                            
+                            bootinfo.memory_info.total_physical_memory += (*mmap).len as usize;
+
+                            if (*mmap).type_ == MULTIBOOT_MEMORY_AVAILABLE {
+                                bootinfo.memory_info.available_memory += (*mmap).len as usize;
+                                bootinfo.memory_info.memory_map[i].base_address = (*mmap).addr as usize;
+                                bootinfo.memory_info.memory_map[i].length = (*mmap).len as usize;
+                                bootinfo.memory_info.memory_map[i].type_ = 0;
+                            }
+                            else {
+                                bootinfo.memory_info.memory_map[i].base_address = (*mmap).addr as usize;
+                                bootinfo.memory_info.memory_map[i].length = (*mmap).len as usize;
+                                bootinfo.memory_info.memory_map[i].type_ = 1;
+                            }
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    i += 1;
+                }
+                bootinfo.memory_info.memory_map_entries = (i) as u16;
+            }
+
+            // Handle tag types we don't care about
+            _ => {
+            }
         }
+        
+        // Index the tag pointer (tag.size) number of bytes forward, ensuring it is 8 byte aligned as per Multiboot 2 spec.
+        tag = unsafe { (tag as usize + ((*tag).size as usize + 7) & !7) as *const _ };
     }
 }
