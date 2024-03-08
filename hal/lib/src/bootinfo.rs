5d2cc089c5d29399ad0997d6ca0a36665263675f
Mike Wyatt <wyattmich61@gmail.com>
2024-03-08 14:27:10 -0500

    gdt implementation fully working now

--- hal/lib/src/bootinfo.rs
+++ hal/lib/src/bootinfo.rs
@@ -182,14 +182,48 @@ pub mod i686 {
         pub gdt_pointer: GDTPointer,
     }
 
+    
+    #[repr(C, packed)]
+    pub struct GDTEntry {
+        limit_low:              u16,
+        base_low:               u16,
+        base_middle:            u8,
+        access:                 u8,
+        limit_high_granularity: u8,
+        base_high:              u8,
+    }
+
+
+    impl GDTEntry {
+        pub fn new(base: u32, limit: u32, access: u8) -> Self {
+            GDTEntry {
+                limit_low: (limit & 0xFFFF) as u16,
+                base_low: (base & 0xFFFF) as u16,
+                base_middle: ((base >> 16) & 0xFF) as u8,
+                access,
+                limit_high_granularity: (0xD << 4) | ((limit & 0x000F) as u8) as u8,     // Granularity is the same for every descriptor: 4KB page granularity, 32 bit protected mode
+                base_high: ((base >> 24) & 0xFF) as u8,
+            }
+        }
+
+        pub fn get_offset(&self, gdt: &GlobalDescriptorTable) -> u16 {
+            // Calculate the offset of the given selector within the struct
+            let base_ptr = gdt as *const GlobalDescriptorTable as usize;
+            let selector_ptr = self as *const GDTEntry as usize;
+            (selector_ptr - base_ptr) as u16
+        }
+    }
+
+
     #[repr(C, packed)]
     pub struct GlobalDescriptorTable {
-        pub null:       GDTEntry,
+        pub null:       u64,
         pub sys_code:   GDTEntry,
         pub sys_data:   GDTEntry,
         pub user_code:  GDTEntry,
         pub user_data:  GDTEntry,
     }
+
 
     impl GlobalDescriptorTable {
         pub fn get_selector_offset(&self, selector: &GDTEntry) -> u16 {
@@ -199,17 +233,66 @@ pub mod i686 {
             (selector_ptr - base_ptr) as u16
         }
     }
-    
 
-    #[repr(C, packed)]
-    pub struct GDTEntry {
-        limit_low:      u16,
-        base_low:       u16,
-        base_middle:    u8,
-        access:         u8,
-        granularity:    u8,
-        base_high:      u8,
+
+    impl Default for GlobalDescriptorTable {
+        
+        fn default() -> Self {
+            const RING0_CODE_SEG_ACCESS: u8 = 0x9A;
+            const RING0_DATA_SEG_ACCESS: u8 = 0x96;
+            const RING3_CODE_SEG_ACCESS: u8 = 0xFA;
+            const RING3_DATA_SEG_ACCESS: u8 = 0xF6;
+
+            Self {
+                null:       0,                                                                // Null segment descriptor
+                sys_code:   GDTEntry::new(0, 0xFFFFFF, RING0_CODE_SEG_ACCESS),                // Code segment descriptor (supervisor, ring 0)
+                sys_data:   GDTEntry::new(0, 0xFFFFFF, RING0_DATA_SEG_ACCESS),                // Data segment descriptor (supervisor, ring 0)
+                user_code:  GDTEntry::new(0, 0xFFFFFF, RING3_CODE_SEG_ACCESS),                // Code segment descriptor (usermode, ring 3)
+                user_data:  GDTEntry::new(0, 0xFFFFFF, RING3_DATA_SEG_ACCESS),                // Data segment descriptor (usermode, ring 3)
+            }
+
+            // Code Segment Access Byte:
+            //  --------------------
+            // | 0 | _00 | 0 | 0000 |
+            // | P | DPL | S | 1CRA |
+            //  --------------------
+            //
+            // P: Present - Always set to 1 as these segments are always available for use
+            // DPL: Descriptor Privilege Level - Which protection ring the descriptor runs as. (R0: 00, R3: 11)
+            // S: Descriptor Type - Differentiates between system and our descriptors, always set to 1
+            // CRA: Conforming, Readable, Accessed
+
+            // Data Segment Access Byte:
+            //  --------------------
+            // | 0 | _00 | 0 | 0000 |
+            // | P | DPL | S | 0EWA |
+            //  --------------------
+            //
+            // P - Present:
+            //      Always set to 1 as these segments are always available for use
+            // DPL - Descriptor Privilege Level:
+            //      Which protection ring the descriptor runs as. (R0: 00, R3: 11)
+            // S - Descriptor Type:
+            //      Differentiates between system descriptors and our descriptors, always set to 1
+            // EWA - Expand, Writable, Accessed:
+            //
+
+            // Granularity byte
+            //  -----------------------------
+            // | 0 | 0 |   0   |  0  |  0000 |
+            // | G | D | RSRVD | AVL |  LMIT |
+            //  -----------------------------
+            //
+            // G - Granularity:
+            //      0 = Limit field interpreted in bytes
+            //      1 = Limit field interpreted in 4KB pages
+            //
+            // D - Default Operation Size:
+            //      0 = 16 bit real mode
+            //      1 = 32 bit protected mode
+        }
     }
+
 
     /// Pointer to the Global Descriptor table
     #[derive(Default)]
@@ -219,41 +302,12 @@ pub mod i686 {
         pub base:   usize,
     }
 
+
     impl GDTPointer {
         pub fn new(base: usize) -> Self {
             GDTPointer {
                 limit: (core::mem::size_of::<GlobalDescriptorTable>() as u16) - 1,
                 base: base,
-            }
-        }
-    }
-
-    impl GDTEntry {
-        pub fn new(base: u32, limit: u32, access: u8, granularity: u8) -> Self {
-            let mut entry = GDTEntry {
-                limit_low: (limit & 0xFFFF) as u16,
-                base_low: (base & 0xFFFF) as u16,
-                base_middle: ((base >> 16) & 0xFF) as u8,
-                access,
-                granularity,
-                base_high: ((base >> 24) & 0xFF) as u8,
-            };
-
-            // Set the granularity bits in the GDT entry
-            entry.granularity |= 0b1100_0000; // Set the flags
-
-            entry
-        }
-    }
-
-    impl Default for GlobalDescriptorTable {
-        fn default() -> Self {
-            Self {
-                null:       GDTEntry::new(0, 0, 0, 0),                  // null segment descriptor
-                sys_code:   GDTEntry::new(0, 0xffffffff, 0x9A, 0xCF),   // Code segment descriptor (supervisor)
-                sys_data:   GDTEntry::new(0, 0xffffffff, 0x92, 0xCF),   // Data segment descriptor (supervisor)
-                user_code:  GDTEntry::new(0, 0, 0x9A, 0xCF),            // Placeholder for user mode code segment descriptor
-                user_data:  GDTEntry::new(0, 0, 0x92, 0xCF),            // Placeholder for user mode data segment descriptor
             }
         }
     }
