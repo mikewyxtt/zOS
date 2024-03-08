3312cf61d15d5a304d8b06b0b150d2b9a4b58d57
Mike Wyatt <wyattmich61@gmail.com>
2024-03-08 06:22:11 -0500

    build load_gdt.S

--- hal/i386/initializer/build.rs
+++ hal/i386/initializer/build.rs
@@ -39,7 +39,45 @@ fn main() {
         std::process::exit(1);
     }
 
-    // Link start.o and use our linker script
+    // build _load_gdt.asm
+
+    // Path to the GAS source file
+    let gas_src_path = "src/load_gdt.S";
+
+    // Retrieve the target directory where the object file will be placed
+    let target_dir = match env::var("OUT_DIR") {
+        Ok(dir) => dir,
+        Err(e) => {
+            eprintln!("Failed to retrieve the target directory (OUT_DIR): {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    // Convert target_dir to a String
+    let target_dir = PathBuf::from(target_dir);
+
+    // Construct the path to the target object file
+    let target_load_gdt_o_path = target_dir.join("load_gdt.o");
+
+    // Run NASM to assemble start.asm into start.o
+    let nasm_output = Command::new("clang")
+        .args(&["-ffreestanding", "-c", "-target", "i686-unknown-none", gas_src_path, "-o"])
+        .arg(&target_load_gdt_o_path) //
+        .output()
+        .expect("Failed to run NASM");
+
+    // Check if NASM produced any error output
+    if !nasm_output.stderr.is_empty() {
+        // Print NASM's error output
+        eprintln!("clang (assembler) error:\n{}", String::from_utf8_lossy(&nasm_output.stderr));
+        // Terminate the build process
+        std::process::exit(1);
+    }
+
+    // Link start.o
     println!("cargo:rustc-link-arg={}", target_start_o_path.display());
+    // Link load_gdt.o
+    println!("cargo:rustc-link-arg={}", target_load_gdt_o_path.display());
+    // Use our linker script
     println!("cargo:rustc-link-arg=-Tlink.ld");
 }
