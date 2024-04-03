6a6c7f27e89e704a6a724c4af9a14e0b4b9e9a81
Michael Wyatt <wyattmich61@gmail.com>
2024-04-02 22:02:13 -0400

    fully working with print macros

--- loader/src/console.rs
+++ loader/src/console.rs
@@ -1,12 +1,60 @@
 use crate::uefi;
+use core::fmt::{Write, Error, self};
 
-pub fn reset() {
-    let efi_system_table = uefi::get_system_table();
-    unsafe { ((*efi_system_table.console_out).reset)((*efi_system_table).console_out, false); }
+
+/// Prints a formatted string with NO trailing newline to console output.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => { ($crate::console::_print(format_args!($($arg)*))); }
 }
 
-pub fn putc(c: char) {
-    let efi_system_table = uefi::get_system_table();
-    let utf16_char: [u16; 2] = [(c as u8).into(), b'\0'.into()];
-    unsafe { ((*efi_system_table.console_out).output_string)((*efi_system_table).console_out, utf16_char.as_ptr()); }
+
+/// Prints a formatted string with a trailing newline to console output.
+#[macro_export]
+macro_rules! println {
+    ($($arg:tt)*) => ($crate::print!("{}\n\r", format_args!($($arg)*)));
+}
+
+
+/// Clears the console
+pub fn reset() {
+    uefi::SimpleTextOutputProtocol::reset();
+}
+
+
+/// Print function that's used by the print macros
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    let mut writer = Writer::new();
+    writer.write_fmt(args).unwrap();
+}
+
+
+/// Writer struct for fmt::Write to use. We don't need to do anything with it other than use it as a place to process the formatted text into, it is simple.
+struct Writer;
+
+impl Writer {
+    /// Returns an empty Writer struct
+    fn new() -> Self {
+        Writer
+    }
+
+    /// Required for the fmt::Write trait. Writes a single byte to the console
+    pub fn write_byte(&mut self, byte: u8) {
+        
+        // UEFI requires UTF-16 string literals. So, we simply create a 16 bit array with the char and '\0' so it thinks it is the end of the string, then call the UEFI output_string function with a pointer to the array. It works...
+        let utf16_str: [u16; 2] = [byte.into(), b'\0'.into()];
+        uefi::SimpleTextOutputProtocol::output_string(utf16_str.as_ptr());
+    }
+}
+
+
+/// In order to use the formatting stuff from the 'core' lib, we must provide the write_str implementation.
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result<(), Error> {
+        for byte in s.bytes() {
+            self.write_byte(byte);
+        }
+        Ok(())
+    }
 }
