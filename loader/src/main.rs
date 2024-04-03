fc7f9d9ae80e5592612a1c9cce360f5ca4d198a9
Michael Wyatt <wyattmich61@gmail.com>
2024-04-02 22:02:38 -0400

    cleaned it up, we have print macros now

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -4,36 +4,22 @@
 mod uefi;
 mod console;
 
+use core::panic::PanicInfo;
+
 #[no_mangle]
-extern "win64" fn efi_main(_efi_image_handle: &usize, system_table: *const uefi::SystemTable) -> ! {
-    uefi::initialize(system_table);
+extern "win64" fn efi_main(_efi_image_handle: *const usize, efi_system_table: *const uefi::SystemTable) -> ! {
+    uefi::initialize(efi_system_table);
     console::reset();
 
-    console::putc('H');
-    console::putc('E');
-    console::putc('L');
-    console::putc('L');
-    console::putc('O');
-    console::putc('\n');
-    console::putc('\r');
-    console::putc('W');
-    console::putc('O');
-    console::putc('R');
-    console::putc('L');
-    console::putc('D');
-    console::putc('!');
+    println!("Yoooo");
+    println!("15 in hex: {:X}", 15);
     
-    unsafe { core::arch::asm!("MOV EAX, 0xFFDD"); }
     loop {}
 }
 
 
-
-use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     unsafe { core::arch::asm!("MOV EAX, 0xBadDeed"); }
     loop {}
 }
-
-
