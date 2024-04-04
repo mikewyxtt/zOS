39f1d33f4bedfd0c00a2838a5ec4a9a809fcea1e
Michael Wyatt <wyattmich61@gmail.com>
2024-04-03 23:46:38 -0400

    more test stuff

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -1,25 +1,32 @@
 #![no_std]
 #![no_main]
 
-mod uefi;
+extern crate alloc;
+
+
+#[macro_use]
 mod console;
+mod uefi;
+mod disk;
+mod allocator;
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
-extern "win64" fn efi_main(_efi_image_handle: *const usize, efi_system_table: *const uefi::SystemTable) -> ! {
-    uefi::initialize(efi_system_table);
+extern "win64" fn efi_main(efi_image_handle: *const usize, efi_system_table: *const uefi::SystemTable) -> ! {
+    uefi::initialize(efi_image_handle, efi_system_table);
     console::reset();
 
-    println!("Yoooo");
-    println!("15 in hex: {:X}", 15);
-    
+    println!("Hello, World!");
+
+    disk::read_blocks();
+
     loop {}
 }
 
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    unsafe { core::arch::asm!("MOV EAX, 0xBadDeed"); }
+    println!("{}", _info);
     loop {}
 }
