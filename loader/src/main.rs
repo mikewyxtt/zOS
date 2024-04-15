aaac97e6f62740cbd509eb0a06d7723a4f6db986
Michael Wyatt <wyattmich61@gmail.com>
2024-04-15 14:56:42 -0400

    WIP

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -9,40 +9,55 @@ extern crate alloc;
 #[macro_use]
 mod allocator;
 mod drivers;
-mod uefi;
+mod libuefi;
 
-use alloc::vec;
-use alloc::vec::Vec;
 use core::panic::PanicInfo;
 use drivers::*;
 
 
-const ROOT_PARTITION: &str = "disk0s2";
-
 #[no_mangle]
-extern "win64" fn efi_main(efi_image_handle: *const usize, efi_system_table: *const uefi::SystemTable) -> ! {
-    uefi::init(efi_image_handle, efi_system_table);
-    console::reset();
-    disk::init().unwrap();
-    get_cfg();
+extern "win64" fn efi_main(efi_image_handle: *const usize, efi_system_table: *const libuefi::SystemTable) -> ! {
+    libuefi::init(efi_image_handle, efi_system_table);
+    console::clear();
+    disk::init();
+
+    drivers::fat32::read();
+    //parse_conf();
 
     println!("Hello, World!");
+    // println!("Boot disk: {}", find_efi_slice().unwrap());
 
-    let mut buffer: Vec<u8> = vec!(0; 512);
-    disk::read_bytes_vec(ROOT_PARTITION, 2,512, &mut buffer).expect("Block IO error");
-    let buffer = buffer;
-
-    let mut ptr = buffer.as_ptr() as usize;
-    ptr += 56;
-    let magic: u16 = unsafe { *(ptr as *const u16) as u16};
-    println!("EXT2 magic: 0x{:X}", magic);
 
     loop {}
 }
 
-fn get_cfg() {
-    //
-}
+// use libuefi::bootservices::BootServices;
+// use libuefi::protocol::{device_path::DevicePathProtocol, loaded_image::LoadedImageProtocol};
+
+// use crate::disk::find_efi_slice;
+
+// fn read_cfg() {
+//     // Find the path to the EFI boot partition
+//     let loaded_image_protocol: *mut *mut LoadedImageProtocol = core::ptr::dangling_mut();
+//     BootServices::handle_protocol(libuefi::IMAGE_HANDLE.load(core::sync::atomic::Ordering::SeqCst), &(LoadedImageProtocol::guid()), loaded_image_protocol.cast());
+//     let loaded_image_protocol: &LoadedImageProtocol = unsafe { &mut (**loaded_image_protocol)};
+
+//     // let file_path = loaded_image_protocol.file_path;
+//     // println!("Dev handle: 0x{:X}", file_path as usize);
+    
+
+//     // let device_path_protocol_ptr: *mut *mut DevicePathProtocol = core::ptr::dangling_mut();
+//     // BootServices::handle_protocol(file_path.cast(), &(DevicePathProtocol::guid()), device_path_protocol_ptr.cast());
+    
+//     let node: &DevicePathProtocol = unsafe { &(*loaded_image_protocol.file_path)};
+//     println!("type: {} subtype:{} length: {}", node._type, node.subtype, node.length[0]);
+//     println!("path loc: 0x{:X}", node as *const DevicePathProtocol as usize);
+// }
+
+// fn parse_conf() {
+//     read_cfg();
+//     // get the values from the config file
+// }
 
 
 #[panic_handler]
