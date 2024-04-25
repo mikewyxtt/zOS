011bb769ce2d47f499aa864fbb676eed66bc5f53
Mike Wyatt <wyattmich61@gmail.com>
2024-04-25 06:43:47 -0400

    added .gitignore to zoslib and removed accidentally tracked build files

--- debugutils/target/debug/deps/debugutils-364655b84429019f.d
+++
@@ -1,5 +0,0 @@
-/home/mike/zOS/debugutils/target/debug/deps/libdebugutils-364655b84429019f.rmeta: src/lib.rs
-
-/home/mike/zOS/debugutils/target/debug/deps/debugutils-364655b84429019f.d: src/lib.rs
-
-src/lib.rs:
--- debugutils/target/debug/deps/debugutils-da6c55186366fdb7.d
+++
@@ -1,5 +0,0 @@
-/home/mike/zOS/debugutils/target/debug/deps/libdebugutils-da6c55186366fdb7.rmeta: src/lib.rs
-
-/home/mike/zOS/debugutils/target/debug/deps/debugutils-da6c55186366fdb7.d: src/lib.rs
-
-src/lib.rs:
