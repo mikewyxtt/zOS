e52b5f8bfae24ab823208174c68510b333e1e31e
Mike Wyatt <wyattmich61@gmail.com>
2024-04-25 06:43:26 -0400

    added license to src files

--- base/loader/src/libuefi/protocol/mod.rs
+++ base/loader/src/libuefi/protocol/mod.rs
@@ -1,3 +1,22 @@
+/*  mod.rs
+ *
+ *  zOS  --  Advanced *NIX System
+ *  Copyright (C) 2024  Free Software Foundation, Inc.
+ *
+ *  zOS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  zOS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with zOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 pub mod block_io;
 pub mod device_path;
 pub mod loaded_image;
