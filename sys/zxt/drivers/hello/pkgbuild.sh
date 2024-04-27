53c72decfd6777a99c864e93298075de6df3952e
Michael Wyatt <wyattmich61@gmail.com>
2024-04-26 23:33:51 -0400

    copies hello world zxt to rootfs

--- sys/zxt/drivers/hello/pkgbuild.sh
+++ sys/zxt/drivers/hello/pkgbuild.sh
@@ -12,9 +12,9 @@ clean() {
     cargo clean
 }
 
-install() {
+build_install() {
     echo
-    #cp target/i686/debug/initializer $zOS_SRC_ROOT/iso/boot/initializer
+    cp target/x86_64-unknown-none/debug/hello $1/hello.zxt
 }
 
 # cd to directory of this package, then execute the function specified as an argument
