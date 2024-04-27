d9bda9a5749bd9d09bebf6722bc596e4b1b9050f
Michael Wyatt <wyattmich61@gmail.com>
2024-04-27 17:08:57 -0400

    simplified utf16 conversion

--- base/loader/src/drivers/uefi/console.rs
+++ base/loader/src/drivers/uefi/console.rs
@@ -22,21 +22,18 @@ use crate::libuefi::protocol::simple_text_output::SimpleTextOutputProtocol;
 
 
 pub fn putc(c: char) {
-    let mut c_u16: [u16; 2] = [0,0];
-    c.encode_utf16(&mut c_u16);
-    
-    // UEFI requires UTF-16 string literals. So, we simply create a 16 bit array with the char and '\0' so it thinks it is the end of the string, then call the UEFI output_string function with a pointer to the array. It works...
-    let utf16_str: [[u16; 2]; 2] = [c_u16, [0,0]];
-    SimpleTextOutputProtocol::output_string(utf16_str[0].as_ptr());
+    // UEFI requires UTF-16 string literals.
+    let mut utf16c = [0; 2];
+    char::encode_utf16(c, &mut utf16c);
+
+    SimpleTextOutputProtocol::output_string(utf16c.as_ptr());
 
     // UEFI is similar to serial in that you have to write the carriage return as well as the newline to reset the cursor
     if c == '\n' {
-        let c = '\r';
-        let mut c_u16: [u16; 2] = [0,0];
-        c.encode_utf16(&mut c_u16);
+        let mut utf16c = [0; 2];
+        char::encode_utf16('\r', &mut utf16c);
 
-        let utf16_str: [[u16; 2]; 2] = [c_u16, [0,0]];
-        SimpleTextOutputProtocol::output_string(utf16_str[0].as_ptr());
+        SimpleTextOutputProtocol::output_string(utf16c.as_ptr());
     }
 }
 
