9ed99e6a01c3b1dc2e2383ebcfd471358b9c90a6
Michael Wyatt <wyattmich61@gmail.com>
2024-05-06 00:05:12 -0400

    much better

--- base/loader/src/drivers/fs.rs
+++ base/loader/src/drivers/fs.rs
@@ -20,14 +20,11 @@
 #![allow(dead_code)]
 
 
-use core::fmt::{Display, Formatter};
-use alloc::{format, string::{String, ToString}, vec::Vec};
-use crate::{extfs, libuefi::{bootservices::BootServices, protocol::{block_io::BlockIOProtocol, device_path::{DevicePathProtocol, HardDriveDevicePath}, file::FileInfo, filesystem::SimpleFilesystem, loaded_image::LoadedImageProtocol}, GUID}};
-
-
-static mut SLICE_ENTRIES: Vec<SliceInfo> = Vec::new();
+use alloc::{string::{String, ToString}, vec::Vec};
+use crate::{extfs, fat, libuefi::{bootservices::BootServices, protocol::{device_path::{DevicePathProtocol, HardDriveDevicePath}, loaded_image::LoadedImageProtocol}, GUID}};
 
 pub const EOF: i8 = -1;
+
 
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -37,81 +34,46 @@ enum FilesystemType {
     UNKNOWN,
 }
 
-impl Display for FilesystemType {
-    fn fmt(&self, _: &mut Formatter<'_>) -> Result<(), core::fmt::Error> { 
-        todo!()
-    }
-}
-
-
-
-#[derive(Clone, Copy)]
-struct SliceInfo {
-    guid:       GUID,
-    handle:     *const usize,
-    fs_type:    FilesystemType,
-    is_esp:     bool,
-}
-
-impl SliceInfo {
-    pub const fn new(guid: GUID, handle: *const usize, fs_type: FilesystemType, is_esp: bool) -> Self {
-        Self {
-            guid,
-            handle,
-            fs_type,
-            is_esp
-        }
-    }
-}
 
 #[derive(PartialEq)]
 pub struct File {
-    slice:              GUID,
-    path:               String,
-    filesize:           u64,
+    pub slice:              GUID,
+    pub path:               String,
+    pub filesize:           u64,
+    _read_raw:              unsafe fn(&Self, usize, *mut u8),
+}
+
+enum DriverInfo {
+    FAT {
+        first_cluster_number:   u32,
+        clusters:               Vec<u32>,
+    }
 }
 
 
 impl File {
+    pub fn new(slice: GUID, path: &str, filesize: u64, read_raw: unsafe fn(&Self, usize, *mut u8)) -> Self {
+        Self {
+            slice,
+            path: path.to_string(),
+            filesize,
+            _read_raw:  read_raw,
+        }
+    }
+
     /// Opens a file, reading its entire contents into a vector
     pub fn open(slice: GUID, path: &str) -> Self {
-
-        let slice = find_slice(slice);
-
-        match slice.fs_type {
+        match detect_fs_type(slice) {
             FilesystemType::FAT => {
-                // UEFI paths are Microsoft style '\' rather than '/'
-                let path = String::from(format!("{}\0", path.to_string().to_ascii_uppercase().replace("/", "\\")));
-
-                // First we need to access the filesystem protocol to open the root directory
-                let filesys_protocol = BootServices::handle_protocol::<SimpleFilesystem>(slice.handle);
-                let efi_file = filesys_protocol.open_volume();
-              
-                // Now we can open the file
-                let efi_file = efi_file.open(path.as_str(), 1, None);
-                let info = efi_file.get_info(FileInfo::guid());                
-
-                efi_file.close();
-
-                return Self {
-                    slice:      slice.guid,
-                    path:       path,
-                    filesize:   info.file_size,
-                }
+                return fat::open(slice, path);
             }
 
             FilesystemType::EXT => {
-                let filesize = 0;
-
-                return Self {
-                    slice:      slice.guid,
-                    path:       path.to_string(),
-                    filesize:   filesize,
-                }
+                return extfs::open(slice, path);
             }
 
             FilesystemType::UNKNOWN => {
-                panic!("Trying to open \"{path}\" on slice with GUID {} failed: Unknown filesystem \nHalting.", slice.guid.as_string())
+                panic!("Trying to open \"{path}\" on slice with GUID {} failed: Unknown filesystem \nHalting.", slice.as_string())
             }
         }
     }
@@ -119,40 +81,10 @@ impl File {
     /// Reads 'count' bytes from the file into 'buffer'. Returns the amount of bytes that were read
     pub unsafe fn read_raw(&self, count: usize, buffer: *mut u8) -> usize {
         assert!(count <= self.filesize as usize);
+        
+        (self._read_raw)(&self, count, buffer);
 
-        let fs_type = find_slice(self.slice).fs_type;
-        match fs_type {
-            FilesystemType::FAT => {
-                // UEFI paths are Microsoft style '\' rather than '/'
-                let path = String::from(format!("{}\0", self.path.to_string().to_ascii_uppercase().replace("/", "\\")));
-
-                // First we need to access the filesystem protocol to open the root directory
-                let filesys_protocol = BootServices::handle_protocol::<SimpleFilesystem>(find_slice(self.slice).handle);
-                let file = filesys_protocol.open_volume();
-              
-                // Now we can open the file
-                let file = file.open(path.as_str(), 1, None);
-
-                let mut efi_count = count;
-                unsafe { 
-                    file.read(&mut efi_count, buffer);
-                }
-
-                file.close();
-
-                efi_count
-
-            }
-
-            FilesystemType::EXT => {
-                //
-                0
-            }
-
-            FilesystemType::UNKNOWN => {
-                panic!("Error: Cannot read '{}' from slice with GUID {}: File not found \nHalting.", self.path, self.slice.as_string())
-            }
-        }
+        count
     }
 
     /// Reads the entire contents of the file into a String
@@ -176,35 +108,31 @@ impl File {
 
 
 
-
-/// Finds a slice by GUID
-fn find_slice(guid: GUID) -> SliceInfo {
-    unsafe {
-        for slice in SLICE_ENTRIES.iter() {
-            if slice.guid == guid {
-                return *slice
-            }
-        }
-
-        panic!("FS error: Could not find slice with GUID: {}. Halting.", guid.as_string());
-    }
-}
-
-
 /// Returns the GUID partition signature of the ESP
 pub fn get_esp_guid() -> GUID {
-    unsafe {
-        for slice in SLICE_ENTRIES.iter() {
-            if slice.is_esp {
-                return slice.guid
+    let handle = BootServices::handle_protocol::<LoadedImageProtocol>(crate::libuefi::IMAGE_HANDLE.load(core::sync::atomic::Ordering::SeqCst)).device_handle;
+    let mut node = BootServices::handle_protocol::<DevicePathProtocol>(handle as *const usize);
+
+    while (node._type, node.subtype) != (0x7F, 0xFF)  {
+                match (node._type, node.subtype, node.length[0] + node.length[1]) {
+                    // Hard drive device path
+                    (4, 1, 42) => {
+                        // Cast the current node as HardDriveDevicePath so we can read the GUID
+                        #[allow(invalid_reference_casting)]
+                        let hddp: &HardDriveDevicePath = unsafe { &*((node as *const DevicePathProtocol).cast()) };
+                        let guid = hddp.partition_sig;
+
+                        return guid;
+                    }
+    
+                    _ => {}
+                }
+    
+                node = node.next();
             }
-        }
-        
-    }
 
     panic!("FS error: Could not find EFI System Partition. Halting.");
 }
-
 
 
 
@@ -212,6 +140,9 @@ pub fn get_esp_guid() -> GUID {
 fn detect_fs_type(guid: GUID) -> FilesystemType {
     if extfs::detect(guid) {
         return FilesystemType::EXT;
+    }
+    else if fat::detect(guid) {
+        return FilesystemType::FAT;
     }
     else {
         return FilesystemType::UNKNOWN;
@@ -223,48 +154,5 @@ fn detect_fs_type(guid: GUID) -> FilesystemType {
 
 /// Start the filesystem driver
 pub fn start() {
-    // Get the EFI_HANDLE of the slice containing the EFI System Partition so we can label its FSInfo entry as such. It's important to note that multiple ESPs may be deteced, e.g if the user is booting from a memory stick
-    // but also has a ESP on their primary hard disk. Matching the device handle of the boot slice to the partition signature of the slice is a reliable way to ensure we found the correct ESP, as opposed to searching for the ESP magic GUID
-    let efi_sys_handle = BootServices::handle_protocol::<LoadedImageProtocol>(crate::libuefi::IMAGE_HANDLE.load(core::sync::atomic::Ordering::SeqCst)).device_handle;
-
-
-    // Get a list of handles that support the BlockIOProtocol. This list includes every storage media device + their partitions.
-    let handles = BootServices::locate_handle_by_protocol::<BlockIOProtocol>();
-    
-    // Iterate through the handles, looking specifically for the Hard Drive Device Path node. The presence of this node indicates that the handle belongs to a slice
-    let mut slice_info: Vec<SliceInfo> = Vec::new();
-
-    for i in 0..handles.len() {
-        let mut node = BootServices::handle_protocol::<DevicePathProtocol>(handles[i] as *const usize);
-
-        while (node._type, node.subtype) != (0x7F, 0xFF)  {
-            match (node._type, node.subtype, node.length[0] + node.length[1]) {
-                // Hard drive device path
-                (4, 1, 42) => {
-                    // Cast the current node as HardDriveDevicePath so we can read the GUID
-                    #[allow(invalid_reference_casting)]
-                    let hddp: &HardDriveDevicePath = unsafe { &*((node as *const DevicePathProtocol).cast()) };
-                    let guid = hddp.partition_sig;
-            
-            
-                    // See if we found the ESP or not
-                    if handles[i] == efi_sys_handle as usize {
-                        slice_info.push(SliceInfo::new(guid, handles[i] as *const usize, FilesystemType::FAT, true));
-                        ldrprintln!("Found EFI System Partion with GUID: {}", guid.as_string());
-                    }
-                    else {
-                        slice_info.push(SliceInfo::new(guid, handles[i] as *const usize, detect_fs_type(guid), false));
-                        ldrprintln!("Found slice with GUID: {}", guid.as_string());
-                    }
-                }
-
-                _ => {}
-            }
-
-            node = node.next();
-        }
-    }
-
-    assert_ne!(slice_info.is_empty(), true);
-    unsafe {SLICE_ENTRIES = slice_info.clone(); }
+    // Do something
 }
