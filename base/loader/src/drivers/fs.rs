bfd6cabf64f0cf6311ac795e06a8c6b4de4fe258
Michael Wyatt <wyattmich61@gmail.com>
2024-05-09 02:17:15 -0400

    WIP

--- base/loader/src/drivers/fs.rs
+++ base/loader/src/drivers/fs.rs
@@ -20,7 +20,7 @@
 #![allow(dead_code)]
 
 
-use alloc::{string::String, vec::Vec, vec};
+use alloc::{string::{String, ToString}, vec, vec::Vec};
 use crate::{extfs, fat, libuefi::{bootservices::BootServices, protocol::{device_path::{DevicePathProtocol, HardDriveDevicePath}, loaded_image::LoadedImageProtocol}, GUID}};
 
 
@@ -124,4 +124,128 @@ pub fn read_to_string(slice: GUID, path: &str) -> String {
 /// Start the filesystem driver
 pub fn start() {
     // Do something
+}
+
+
+// Generic filetype, used inside all other file types
+pub struct File {
+    slice:  GUID,
+    uuid:   Option<u128>,
+    path:   String,
+}
+
+impl File {
+    pub fn open_by_guid(slice: GUID, path: &str) -> Self {
+        Self {
+            slice:  slice,
+            uuid:   None,
+            path:   path.to_string(),
+        }
+    }
+
+    pub fn open_by_uuid(uuid: u128, path: &str) -> Self {
+        let slice = get_esp_guid();
+
+        Self {
+            slice:  slice,
+            uuid:   Some(uuid),
+            path:   path.to_string(),
+        }
+    }
+
+    /// Reads a files entire contents into *buffer*
+    ///
+    /// If *buffer* is a null ptr, this fn returns the buffer size needed to contain the file. Otherwise, it returns None.
+    pub unsafe fn read_raw(&self, buffer: *mut u8) -> Option<u64> {
+        match detect_fs_type(self.slice) {
+            FilesystemType::FAT => {
+                return fat::read_bytes_raw(self.slice, &self.path, buffer);
+            }
+
+            FilesystemType::EXT => {
+                // return extfs::read_bytes_raw(slice, path, buffer);
+                return None;
+            }
+
+            FilesystemType::UNKNOWN => {
+                let path = &self.path;
+                if self.uuid.is_some() {
+                    panic!("Trying to open \"{path}\" on slice with UUID {} failed: Unknown filesystem \nHalting.", self.uuid.unwrap())
+                }
+                else {
+                    panic!("Trying to open \"{path}\" on slice with GUID {} failed: Unknown filesystem \nHalting.", self.slice.as_string())
+                }
+            }
+        }
+    }
+
+    /// Reads the entire contents of the file into a String
+    pub fn read_to_string(&self) -> Result<String, ()> {
+        let filesize = unsafe { self.read_raw(core::ptr::null_mut()).unwrap() };
+        let contents: Vec<u8> = { 
+            let mut buffer: Vec<u8> = vec![0; filesize.try_into().unwrap()];
+            unsafe { self.read_raw(buffer.as_mut_ptr()) };
+
+            buffer
+        };
+
+        let mut s = String::new();
+
+        for b in &contents {
+            s.push(*b as char);
+        }
+
+        Ok(s)
+    }
+}
+
+
+
+// Firwmare has everything needed to load files and print to screen, no drivers needed
+mod firmware {
+    //
+    pub mod disk {
+        pub struct Disk {
+            pub block_size: u32,
+        }
+    }
+
+    pub mod console {
+        pub fn clear() {
+            //
+        }
+
+        pub fn _print() {
+            //
+        }
+    }
+
+    pub mod arch {
+        use crate::libuefi::{bootservices::BootServices, protocol::{device_path::{DevicePathProtocol, HardDriveDevicePath}, loaded_image::LoadedImageProtocol}, GUID};
+
+        pub fn get_esp_guid() -> GUID {
+            let handle = BootServices::handle_protocol::<LoadedImageProtocol>(crate::libuefi::IMAGE_HANDLE.load(core::sync::atomic::Ordering::SeqCst)).device_handle;
+            let mut node = BootServices::handle_protocol::<DevicePathProtocol>(handle as *const usize);
+        
+            while (node._type, node.subtype) != (0x7F, 0xFF)  {
+                        match (node._type, node.subtype, node.length[0] + node.length[1]) {
+                            // Hard drive device path
+                            (4, 1, 42) => {
+                                // Cast the current node as HardDriveDevicePath so we can read the GUID
+                                #[allow(invalid_reference_casting)]
+                                let hddp: &HardDriveDevicePath = unsafe { &*((node as *const DevicePathProtocol).cast()) };
+                                let guid = hddp.partition_sig;
+        
+                                return guid;
+                            }
+            
+                            _ => {}
+                        }
+            
+                        node = node.next();
+                    }
+        
+            panic!("FS error: Could not find EFI System Partition. Halting.");
+        }
+    }
 }
